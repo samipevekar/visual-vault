f55cf962336c8ae990c8768e48a995575f15bd2b
samipevekar <samipevekar499@gmail.com>
2024-05-14 18:33:08 +0530

    some changes made for socket

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -21,7 +21,7 @@ io.on('connection',(socket)=>{
     console.log("a user connected",socket.id)
 
     const userId = socket.handshake.query.userId;
-    if(userId != "undefined") userSocketMap[userId] = socket.id;
+    if(userId !== undefined && userId != "undefined") userSocketMap[userId] = socket.id;
 
     // io.emit() is used to send events to all connected clients
     io.emit("getOnlineUsers",Object.keys(userSocketMap))
