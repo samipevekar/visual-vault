8d18032701577ac80417dff849a4b715112f6a5a
sami pevekar <sami@p.com>
2024-05-14 19:47:11 +0530

    some changes made for socket

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,9 +8,7 @@ const io = new Server(server, {
   cors: {
     origin: ["https://visual-vault-app.vercel.app", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["content-type"],
     credentials: true,
-    optionsSuccessStatus: 200,
   },
 });
 
@@ -24,7 +22,7 @@ io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId !== undefined && userId != "undefined") userSocketMap[userId] = socket.id;
+  if (userId !== "undefined") userSocketMap[userId] = socket.id;
 
   // io.emit() is used to send events to all connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
