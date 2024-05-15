9aefc72d3dd30858cfe7e5cbaef881c8bec81cd2
sami pevekar <sami@p.com>
2024-05-15 09:15:13 +0530

    some changes made for socket

--- frontend/src/Context/ContextApi.jsx
+++ frontend/src/Context/ContextApi.jsx
@@ -7,8 +7,8 @@ import io from "socket.io-client"
 
 export default function ContextApi(props) {
 
-  const HOST = "http://localhost:4000" 
-  // const HOST = "https://visual-vault-indol.vercel.app" 
+  // const HOST = "http://localhost:4000" 
+  const HOST = "https://visual-vault-indol.vercel.app" 
 
     // creating sidebar with toggle button
     const [isOpen,setIsOpen] = useState(false)  
@@ -144,7 +144,7 @@ export default function ContextApi(props) {
 
     useEffect(()=>{
         if(userInfo){
-            const socket = io(HOST,{
+            const socket = io(`${HOST}`,{
                 query:{
                     userId:userInfo._id
                 }
