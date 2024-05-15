113e9f020603f1b847cc4f2de11c071a8bcefa61
sami pevekar <sami@p.com>
2024-05-15 09:00:47 +0530

    some changes made for socket

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken")
-const JWT_SECRET = process.env.JWT_SIGN
+const JWT_SECRET = "unknownproject"
 
 const fetchuser = (req,res,next)=>{
     const token = req.header("auth-token")
