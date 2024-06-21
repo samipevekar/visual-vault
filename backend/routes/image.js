75c0c94cdcfacf9912eee20d1f14b4e568060eb9
sami pevekar <sami@p.com>
2024-06-21 16:56:06 +0530

    ready to deploy

--- backend/routes/image.js
+++ backend/routes/image.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const fetchuser = require("../middleware/middleware");
@@ -6,12 +7,22 @@ const multer = require("multer");
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebase/serviceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'gs://visual-vault-images.appspot.com',
-});
+    credential: admin.credential.cert({
+      type: process.env.GOOGLE_TYPE,
+      project_id: process.env.GOOGLE_PROJECT_ID,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      auth_uri: process.env.GOOGLE_AUTH_URI,
+      token_uri: process.env.GOOGLE_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+      universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
+    }),
+    storageBucket: 'gs://visual-vault-images.appspot.com',
+  });
 
 const bucket = admin.storage().bucket();
 const upload = multer({ storage: multer.memoryStorage() });
